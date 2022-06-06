6f18c001f4f57825ec510ce89516022f9219e334
Maroussia Arnault <maroussia.arnault@gmail.com>
2022-06-06 17:33:15 +0200

    Added error handling

--- index.js
+++ index.js
@@ -20,17 +20,29 @@ app.post("/scan", (req, res) => {
   const url = req.body.url;
 
   // If the input is null return "Empty Data" error
-  if (url.length === 0) res.send("Empty Data!");
+  if (url.length === 0 || url.match(" ")) res.send("Error is occured");
 
   // Let us convert the input stored in the url and return it as a representation of the QR Code image contained in the Data URI(Uniform Resource Identifier)
   // It shall be returned as a png image format
   // In case of an error, it will save the error inside the "err" variable and display it
-  qrcode.toDataURL(url, (err, src) => {
-    if (err) res.send("Error occurred");
+  // qrcode.toDataURL(url, (err, src) => {
+  //   if (err) res.send("Error occurred");
 
-    // Let us return the QR code image as our response and set it to be the source used in the webpage
-    res.render("scan", { src });
-  });
+  //   // Let us return the QR code image as our response and set it to be the source used in the webpage
+  //   res.render("scan", { src });
+  // });
+
+  qrcode.toDataURL(url)
+    .then(src => {
+      console.log("Url: " + url);
+      console.log(src);
+      // Let us return the QR code image as our response and set it to be the source used in the webpage
+      res.render("scan", { src });
+    })
+    .catch(err => {
+      console.error(err);
+      res.render("error", { err });
+    })
 });
 
 // Setting up the port for listening requests
